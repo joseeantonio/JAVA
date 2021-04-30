0c0d99b50a9c0a19eaf3bcfeeedcb72b7c202c2c
jose antonio <gonzalezperezjoseant@gmail.com>
2021-04-30 12:21:31 +0200

    Actividad 6 realizada

---
+++ Actividades5_2/src/Actividad_6/rectangulo_main.java
@@ -0,0 +1,9 @@
+package Actividad_6;
+
+public class rectangulo_main {
+    public static void main(String[] args){
+            Rectangulo rectangulo1 = new Rectangulo(7, 9);
+
+            System.out.println("El area del rectangulo es: "+rectangulo1.area_rect());
+    }
+}
