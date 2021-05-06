b34b6674659630853552e6b99334cbad7b07efde
jose antonio <gonzalezperezjoseant@gmail.com>
2021-05-06 10:49:34 +0200

    Actividad 7 realizada

--- Actividades5_2/src/Actividad_7/coche_main.java
+++ Actividades5_2/src/Actividad_7/coche_main.java
@@ -2,9 +2,17 @@ package Actividad_7;
 
 public class coche_main {
     public static void main(String[] args){
+
         coche coche1 = new coche("rosa","Seat","Seat arona",23,4,434534);
 
+        System.out.println("El coche 1 es : "+coche1);
 
-        System.out.println(coche1);
+
+        coche coche2 = new coche("negro","Opel","Opel corsa",35,2,123456);
+
+        System.out.println("El coche 2 es : "+coche2);
+
+        coche2.setColor("Amarillo");
+        System.out.println("El coche 2 modificado es : "+coche2);
     }
 }
