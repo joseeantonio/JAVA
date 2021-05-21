dd2316a48b5bb838eb6f95d1a4c85220fab5038e
jose antonio <gonzalezperezjoseant@gmail.com>
2021-05-21 11:57:48 +0200

    push

--- Actividades5_2/src/Actividad_9/Persona_main.java
+++ Actividades5_2/src/Actividad_9/Persona_main.java
@@ -6,7 +6,7 @@ public class Persona_main {
         Persona persona1 = new Persona ("3454353B");
 
         Cuenta cuenta1 = new Cuenta(0,0);
-        Cuenta cuenta2 = new Cuenta(1,700);
+        Cuenta cuenta2 = new Cuenta(1, 700);
 
         persona1.agregarCuenta(cuenta1);
         persona1.agregarCuenta(cuenta2);
@@ -22,6 +22,9 @@ public class Persona_main {
         System.out.println(cuenta1);
         System.out.println(cuenta2.getSaldo_disponible());
 
+
+        System.out.println(cuenta1.consultar_saldo_disponible());
+        System.out.println(cuenta2.consultar_saldo_disponible());
 
         if (persona1.persona_morosa()){
             System.out.println("La persona es morosa");
